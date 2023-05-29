ec97bc1feec911d9722ddab028e1540b3811c1bb
I don't now how to programming <98327880+lvl1245@users.noreply.github.com>
2023-05-30 00:27:43 +0300

    small fix2

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class JunitTests {
 
  @Test
- void Testss() {
+ void TestSolveBrackets() {
      assertAll( () ->assertEquals("Последовательность верная! ([])[]()(([]))" ,App.SolveBrackets("([])[]()(([]))")),
                 () ->assertEquals("Добавьте открывающие скобки!" ,App.SolveBrackets("([)]")),
                 () ->assertEquals("Последовательность верная! {{}}([][])" ,App.SolveBrackets("{{}}([][])")),
